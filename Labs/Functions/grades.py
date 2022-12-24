470d6713a9f86ae738e84e6ec6c1fd6e5c8db8d8
stivistr <stivan_strahilov@abv.bg>
2022-12-24 11:47:58 +0200

    Create grades.py

---
+++ Labs/Functions/grades.py
@@ -0,0 +1,15 @@
+def grade_table(grade):
+    if 2.00 <= grade <= 2.99:
+        print("Fail")
+    elif 3.00 <= grade <= 3.49:
+        print("Poor")
+    elif 3.50 <= grade <= 4.49:
+        print("Good")
+    elif 4.50 <= grade <= 5.49:
+        print("Very Good")
+    elif 5.50 <= grade <= 6.00:
+        print("Excellent")
+
+
+current_grade = float(input())
+grade_table(current_grade)
